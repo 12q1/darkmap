e6d999b94fe0c4497a1b7bee674e7db1a33750b6
12q1 <thomasham89@gmail.com>
2021-09-02 15:26:04 +0200

    preparing to host on gh-pages

--- src/App.js
+++ src/App.js
@@ -5,11 +5,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <MapContainer style={{ height: "100vh", width: "100vw" }} center={[59.329, 18.068]} zoom={13} scrollWheelZoom={true} zoomControl={false} attributionControl={false}>
+      <MapContainer style={{ height: "100vh", width: "100vw" }} center={[35.652, 139.839]} zoom={13} scrollWheelZoom={true} zoomControl={false} attributionControl={false}>
         <TileLayer
-          //attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://api.mapbox.com/styles/v1/12q1/ckt27sq5h04jx18lqr5q9vkvr/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiMTJxMSIsImEiOiJja3QyNGt1MWgwZ3czMnd1bGZtYmNmMnM3In0.A-76Dmcwo88DHHV4dkBvTA"
+          
+          //monochrome original
           //url="https://api.mapbox.com/styles/v1/12q1/ckt27ipwl14zj17q911begbtj/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiMTJxMSIsImEiOiJja3QyNGt1MWgwZ3czMnd1bGZtYmNmMnM3In0.A-76Dmcwo88DHHV4dkBvTA"
+          
+          //lightblue
           //url="https://api.mapbox.com/styles/v1/12q1/ckt24h5fa01c417p7l9cyu9ia/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiMTJxMSIsImEiOiJja3QyNGt1MWgwZ3czMnd1bGZtYmNmMnM3In0.A-76Dmcwo88DHHV4dkBvTA"
         />
       </MapContainer>

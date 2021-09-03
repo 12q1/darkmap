b12f298a2baa114fe25389ea8c513ef6719cf460
12q1 <thomasham89@gmail.com>
2021-09-03 03:04:06 +0200

    superscale prototype

--- src/App.js
+++ src/App.js
@@ -5,17 +5,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <MapContainer style={{ height: "100vh", width: "100vw" }} center={[35.652, 139.839]} zoom={13} scrollWheelZoom={true} zoomControl={false} attributionControl={false}>
-        <TileLayer
-          url="https://api.mapbox.com/styles/v1/12q1/ckt27sq5h04jx18lqr5q9vkvr/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiMTJxMSIsImEiOiJja3QyNGt1MWgwZ3czMnd1bGZtYmNmMnM3In0.A-76Dmcwo88DHHV4dkBvTA"
-          
+      <div className="leafletContainerContainer">
+        <MapContainer className="leafletContainer" center={[40.730, -73.935]} zoom={13} scrollWheelZoom={true} zoomControl={false} attributionControl={false}>
+          <TileLayer
+            url="https://api.mapbox.com/styles/v1/12q1/ckt27sq5h04jx18lqr5q9vkvr/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiMTJxMSIsImEiOiJja3QyNGt1MWgwZ3czMnd1bGZtYmNmMnM3In0.A-76Dmcwo88DHHV4dkBvTA"
+
           //monochrome original
           //url="https://api.mapbox.com/styles/v1/12q1/ckt27ipwl14zj17q911begbtj/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiMTJxMSIsImEiOiJja3QyNGt1MWgwZ3czMnd1bGZtYmNmMnM3In0.A-76Dmcwo88DHHV4dkBvTA"
-          
+
           //lightblue
           //url="https://api.mapbox.com/styles/v1/12q1/ckt24h5fa01c417p7l9cyu9ia/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiMTJxMSIsImEiOiJja3QyNGt1MWgwZ3czMnd1bGZtYmNmMnM3In0.A-76Dmcwo88DHHV4dkBvTA"
-        />
-      </MapContainer>
+          />
+        </MapContainer>
+      </div>
     </div>
   );
 }
